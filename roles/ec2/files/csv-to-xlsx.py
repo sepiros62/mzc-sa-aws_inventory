91152ffa80195f4adda6968a9e42ac76f1539096
jaehwan <sepiros62@gmail.com>
2020-10-05 16:30:45 +0900

    Update | convert tasks #5

--- roles/ec2/files/csv-to-xlsx.py
+++ roles/ec2/files/csv-to-xlsx.py
@@ -92,7 +92,7 @@ ws6.title = "ROUTE53"
 ws6['A1'] = '[AWS] EC2 Resource'
 
 excel_initialized(ws1)
-with open('ec2-template.csv', 'r') as f:
+with open('ec2-report.csv', 'r') as f:
     reader = csv.reader(f)
     for r, row in enumerate(reader):
         for c, col in enumerate(row):
@@ -102,7 +102,7 @@ with open('ec2-template.csv', 'r') as f:
     wb.save('filename = dest_filename')
 
 excel_initialized(ws2)
-with open('vpc-template.csv', 'r') as f:
+with open('vpc-report.csv', 'r') as f:
     reader = csv.reader(f)
     for r, row in enumerate(reader):
         for c, col in enumerate(row):
@@ -112,7 +112,7 @@ with open('vpc-template.csv', 'r') as f:
     wb.save('filename = dest_filename')
 
 excel_initialized(ws3)
-with open('s3-template.csv', 'r') as f:
+with open('s3-report.csv', 'r') as f:
     reader = csv.reader(f)
     for r, row in enumerate(reader):
         for c, col in enumerate(row):
@@ -122,7 +122,7 @@ with open('s3-template.csv', 'r') as f:
     wb.save('filename = dest_filename')
 
 excel_initialized(ws4)
-with open('cf-template.csv', 'r') as f:
+with open('cf-report.csv', 'r') as f:
     reader = csv.reader(f)
     for r, row in enumerate(reader):
         for c, col in enumerate(row):
@@ -132,7 +132,7 @@ with open('cf-template.csv', 'r') as f:
     wb.save('filename = dest_filename')
 
 excel_initialized(ws5)
-with open('rds-template.csv', 'r') as f:
+with open('rds-report.csv', 'r') as f:
     reader = csv.reader(f)
     for r, row in enumerate(reader):
         for c, col in enumerate(row):
@@ -142,7 +142,7 @@ with open('rds-template.csv', 'r') as f:
     wb.save('filename = dest_filename')
 
 excel_initialized(ws6)
-with open('route53-template.csv', 'r') as f:
+with open('route53-report.csv', 'r') as f:
     reader = csv.reader(f)
     for r, row in enumerate(reader):
         for c, col in enumerate(row):
